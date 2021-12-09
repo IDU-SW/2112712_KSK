ec0633e292fb0d38c3b6f1c9f09e3d66a39af649
ericabyss <trond746@gmail.com>
2021-12-10 01:11:20 +0900

    _

---
+++ src/pages/info.tsx
@@ -0,0 +1,27 @@
+import React, { FunctionComponent } from 'react'
+import { graphql } from 'gatsby'
+import Text from 'components/Text'
+
+type InfoPageProps = {}
+
+const InfoPage: FunctionComponent<InfoPageProps> = function () {
+  return (
+    <div>
+      <Text text="Hello" />
+    </div>
+  )
+}
+
+export default InfoPage
+
+export const metadataQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+      }
+    }
+  }
+`
