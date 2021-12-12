256610b5fcfa94c16ac52787ccf6871cfb97c596
ericabyss <trond746@gmail.com>
2021-12-13 01:37:21 +0900

    profile img add

---
+++ src/components/Main/ProfileImage.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+
+//자신이 원하는 프로필 이미지 링크로 설정해주세요.
+const PROFILE_IMAGE_LINK = 
+    '<https://avatars.githubusercontent.com/u/42338190?s=400&u=da8964620e8e676ba45ed33fac68bb14e0d1b1f5&v=4>'
+
+const ProfileImageWrapper = styled.img`
+    width: 120px;
+    height: 120px;
+    margin-bottom: 30px;
+    border-radius: 50%;
+`
+
+const ProfileImage: FunctionComponent = function () {
+    return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+}
+
+export default ProfileImage
