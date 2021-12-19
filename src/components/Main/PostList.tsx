d552d421e31224dc54f7d97daf10e8c0a9303bdc
ericabyss <trond746@gmail.com>
2021-12-19 15:01:33 +0900

    cate

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -9,17 +9,23 @@ const POST_ITEM_DATA = {
     summary:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
     thumbnail:
-      '<https://ji5485.github.io/static/e4f34c558ae8e8235ff53b0311085796/4d854/javascript-core-concept-summary-function-1.webp>',
-    link: '<https://www.google.co.kr/>',
+      'https://ji5485.github.io/static/e4f34c558ae8e8235ff53b0311085796/4d854/javascript-core-concept-summary-function-1.webp',
+    link: 'https://www.google.co.kr/',
   }
 
-const PostListWrapper = styled.div`
-    display:grid;
-    grid-tamplate-columns: 1fr 1fr;
-    grid-gap: 20px;
-    width: 768px;
-    margin: 0 auto;
-    padding: 50px 0 100px;
+  const PostListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  width: 768px;
+  margin: 0 auto;
+  padding: 50px 0 100px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    width: 100%;
+    padding: 50px 20px;
+  }
 `
 
 const PostList: FunctionComponent = function () {
--- src/components/Main/ProfileImage.tsx
+++ src/components/Main/ProfileImage.tsx
@@ -3,14 +3,19 @@ import styled from '@emotion/styled'
 
 //자신이 원하는 프로필 이미지 링크로 설정해주세요.
 const PROFILE_IMAGE_LINK = 
-    '<https://avatars.githubusercontent.com/u/42338190?s=400&u=da8964620e8e676ba45ed33fac68bb14e0d1b1f5&v=4>'
+    'https://avatars.githubusercontent.com/u/42338190?s=400&u=da8964620e8e676ba45ed33fac68bb14e0d1b1f5&v=4'
 
-const ProfileImageWrapper = styled.img`
+    const ProfileImageWrapper = styled.img`
     width: 120px;
     height: 120px;
     margin-bottom: 30px;
     border-radius: 50%;
-`
+  
+    @media (max-width: 768px) {
+      width: 80px;
+      height: 80px;
+    }
+  `
 
 const ProfileImage: FunctionComponent = function () {
     return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
